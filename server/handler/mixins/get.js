cfa55ded02f7002d92962d4404cbae10b11d47cf
antonmyronyuk <antonmyronyuk@gmail.com>
2019-06-16 20:28:22 +0300

    Replace Default handler with action-mixins (for better reusability)

---
+++ server/handler/mixins/get.js
@@ -0,0 +1,17 @@
+const { getStream } = require('../../util/file_repository');
+const { join } = require('path');
+
+const GetMixin = {
+  GET: {
+    handle: async (cmd) => {
+      console.log(cmd);
+
+      const path = join(cmd.username, cmd.filename);
+      const stream = await getStream(path);
+
+      return stream;
+    },
+  },
+};
+
+module.exports = GetMixin;
